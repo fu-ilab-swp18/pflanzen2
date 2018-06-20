4a49ba6873ebe17ff471f49c6658050714014b9a
Lukas Römer <lukasleonroemer@gmail.com>
2018-06-20 23:10:14 +0200

    publish box names via config

--- pi-code/project/src/config.py
+++ pi-code/project/src/config.py
@@ -2,22 +2,29 @@ import yaml
 
 
 with open("../config.yml", 'r') as ymlfile:
-    mappings = yaml.load(ymlfile)['mapping']
+    config = yaml.load(ymlfile)
 
 
 def getBoxID(ip_address):
-    for mapping in mappings:
+    for mapping in config['mapping']:
         if mapping['address'] == ip_address:
             return mapping['boxID']
 
 def getBoxName(ip_address):
-    for mapping in mappings:
+    for mapping in config['mapping']:
         if mapping['address'] == ip_address:
             return mapping['name']
 
 def getSensors(ip_address, type):
-    for mapping in mappings:
+    for mapping in config['mapping']:
         if mapping['address'] == ip_address:
             for sensor in mapping['sensors']:
                 if type == sensor['type']:
                     return sensor['id']
+
+def getBoxNames():
+    boxNames = []
+    for box in config['mapping']:
+        boxNames.append(box['name'])
+    
+    return boxNames
--- pi-code/project/src/mqttClient.py
+++ pi-code/project/src/mqttClient.py
@@ -2,29 +2,46 @@ import paho.mqtt.client as mqtt
 import Queue as queue
 import time
 import json
+import yaml
 
-import mapping
+import config
 
 
 CONFIG_TOPIC = 'SWP_IK_PFL2/config'
+CONFIG_WILDCARD_TOPIC = CONFIG_TOPIC + '/#'
+CONFIG_BOX_NAMES_TOPIC = CONFIG_TOPIC + '/boxnames'
 DATA_TOPIC = 'SWP_IK_PFL2/data'
 BROKER = 'broker.hivemq.com'
 PORT = 1883
 
+
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
 
-    client.subscribe(CONFIG_TOPIC)
+    # client.subscribe(CONFIG_TOPIC)
+    client.subscribe(CONFIG_WILDCARD_TOPIC)
 
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.payload))
 
+    if msg.topic == CONFIG_BOX_NAMES_TOPIC:
+        publishBoxList(client, msg.payload)
+
+
+def publishBoxList(client, payload):
+    try:
+        responseString = {}
+        message = yaml.load(payload)
+        boxNames = config.getBoxNames()
+        responseString['boxNames'] = boxNames
+        client.publish(CONFIG_TOPIC + "/" + message['responseTopic'], yaml.dump(responseString))
+    except Exception as e: print(e)
+
 def mqttWorker(mqttQ):
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
-
     client.connect(BROKER, PORT, 60)
 
     client.loop_start()
@@ -33,13 +50,13 @@ def mqttWorker(mqttQ):
         request = mqttQ.get()
         requestYaml = request[0]
         ipAddress = request[1][0]
-        boxName = mapping.getBoxName(ipAddress)
+        boxName = config.getBoxName(ipAddress)
         print ipAddress
 
         mqttMessage = []
         for sensor in requestYaml['data']:
             mqttSensor = {}
-            sensorID = mapping.getSensors(ipAddress, sensor['type'])
+            sensorID = config.getSensors(ipAddress, sensor['type'])
             if sensorID is not None:
                 mqttSensor['sensor'] = sensorID
                 mqttSensor['value'] = sensor['value']
